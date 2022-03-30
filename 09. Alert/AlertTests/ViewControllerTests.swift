a105ddd3e077ee1bd0f4255d22f73ab650e05165
Phillip Baker <16352712+phillipbaker@users.noreply.github.com>
2022-03-30 13:39:25 +0100

    Complete Chapter 9 tutorial code with updated ViewControllerPresentationSpy framework.
    Remove the old version of the framework. Add the updated version and complete the remaining exercises.

--- 09. Alert/AlertTests/ViewControllerTests.swift
+++ 09. Alert/AlertTests/ViewControllerTests.swift
@@ -10,14 +10,53 @@ import ViewControllerPresentationSpy
 import XCTest
 
 class ViewControllerTests: XCTestCase {
-
-    func test_buttonTap() {
+    
+    private var sut: ViewController!
+    private var alertVerifier: AlertVerifier!
+    
+    override func setUp() {
+        super.setUp()
+        
+        alertVerifier = AlertVerifier()
+        
         let storyboard = UIStoryboard(name: "Main", bundle: nil)
-        let sut: ViewController = storyboard.instantiateViewController(identifier: String(describing: ViewController.self))
+        sut = storyboard.instantiateViewController(identifier: String(describing: ViewController.self))
         
         sut.loadViewIfNeeded()
-        
-        tap(sut.button)
     }
-
+    
+    override func tearDown() {
+        alertVerifier = nil
+        sut = nil
+        super.tearDown()
+    }
+    
+    func test_tappingButton_shouldShowAlert() {
+        tap(sut.button)
+        
+        alertVerifier.verify(title: "Do the thing?",
+                             message: "Let us know if you want to do the thing.",
+                             animated: true,
+                             actions: [
+                                .cancel("Cancel"),
+                                .default("OK")
+                             ],
+                             presentingViewController: sut
+        )
+        
+        XCTAssertEqual(alertVerifier.preferredAction?.title, "OK", "preferred action")
+    }
+    
+    func test_executeAlertAction_withOKButton() throws {
+        tap(sut.button)
+        
+        try alertVerifier.executeAction(forButton: "OK")
+        
+        // Normally, assert something...
+    }
+    
+    func test_executeAlertAction_withCancelButton() throws {
+        tap(sut.button)
+        try alertVerifier.executeAction(forButton: "Cancel")
+    }
 }
